1ce2ae0b467b9072b818bbd8a5fb7e8f529d2ada
krish1729 <krishnacmula@gmail.com>
2023-12-25 15:20:56 +0530

    Added functionality to create rooms and update data in rooms in real time

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Document = require("./Document");
+
+mongoose.connect('mongodb://127.0.0.1:27017/google-docs-clone');
+
+const defaultValue = "";
+
+const io = require("socket.io")(3000, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    },
+})
+
+io.on("connection", socket => { 
+    socket.on('get-document', (documentId) => {
+        const document = findOrCreateDocument(documentId);
+        socket.join(documentId);
+        socket.emit('load-document', document);
+
+        socket.on('send-changes', delta => {
+            socket.broadcast.to(documentId).emit('receive-changes', delta);
+        })
+
+        socket.on('save-document', async data => {
+            await Document.findByIdAndUpdate(documentId, { data });
+        })
+    })
+    
+})
+
+async function findOrCreateDocument(id) {
+    if (id == null) return;
+
+    const document = await Document.findById(id);
+    if (document) return document;
+    return await Document.create({_id: id, data: defaultValue});
+
+}
